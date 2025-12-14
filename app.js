83ab8353b70c04f35ce0d5b756d94e0099ccd64b
RaffayHaroon950 <raffay.haroon950@education.nsw.gov.au>
2025-12-15 01:34:37 +1100

    Tried fixing sw again

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Step 4: Register the service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '/' })
+  navigator.serviceWorker.register('./sw.js')
     .then(() => console.log('Service Worker registered!'))
     .catch(err => console.log('Service Worker failed:', err));
 }
