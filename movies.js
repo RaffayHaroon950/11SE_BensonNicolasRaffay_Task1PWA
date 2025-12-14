ece062689f403dc8fad7607c449f555d1d289244
RaffayHaroon950 <raffay.haroon950@education.nsw.gov.au>
2025-12-14 18:13:44 +1100

    Beginning to query SQL in movies database

--- movies.js
+++ movies.js
@@ -4,8 +4,8 @@ let currentResults = [];
 
 async function loadDatabase() {
     try {
-            const SQL = await initSqlJs({
-                locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/${file}`
+        const SQL = await initSqlJs({
+            locateFile: file => `https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.8.0/${file}`
         });
 
         const response = await fetch('moviesDatabase.db');
@@ -15,6 +15,15 @@ async function loadDatabase() {
         console.log("Database loaded!");
         runQuery(); // initial load
     }   catch (error) {
-            
-        }
+        console.log("Error loading database!")
+        console.error(error)
+    }
+}
+
+function runQuery() {
+    if (!db) return;
+
+    const genre = document.getElementById('genreFilter').value;
+    const age = document.getElementById('ageFilter').value;
+    const rating = document.getElementById('ratingFilter').value;
 }
